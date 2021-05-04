355299812388072370f4c5876975c6308e541d38
siyoon <ysi981123@hanmail.net>
2021-05-04 20:52:10 +0900

    Calendar 변수명 p ->s 로 일괄수정

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "calendar.h"
 
 int main (){
-    Calendar s[100];
+    Calendar p[100];
     int count = 0; 
     int index = 0; 
     int menu;
@@ -22,25 +22,25 @@ int main (){
             }
         }
         if(menu == 1){
-            listPlan(s,index);
+            listPlan(p,index);
         }
         else if(menu == 2){
-            count += createPlan(&s[index++]);
+            count += createPlan(&p[index++]);
         }
         else if(menu == 3){
-            int no = selectDataNo(s,index);
+            int no = selectDataNo(p,index);
             if(no>0){
-                updatePlan(&s[no-1]);
+                updatePlan(&p[no-1]);
             }
         }
         else if(menu == 4){
-            int no = selectDataNo(s,index);
+            int no = selectDataNo(p,index);
             if(no>0){
                 int ok;
                 printf("정말로 삭제하시겠습니까? (삭제를 하시려면 1을 입력하세요)");
                 scanf(" %d",&ok);
                 if(ok == 1){
-                    deletePlan(&s[no-1]);
+                    deletePlan(&p[no-1]);
                     count--;
                 }
             }
