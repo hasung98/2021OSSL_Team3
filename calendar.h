cc7a9b7156b0f6d1fa75f66eb29dc2c59b0ebedb
hasung98 <75055715+hasung98@users.noreply.github.com>
2021-05-04 14:11:19 +0900

    오타수정

--- calendar.h
+++ calendar.h
@@ -2,18 +2,18 @@ typedef struct{
   int year; //년
   int month;//월
   int day;//일
-  char type[100] // 과제 or 시험 or 팀프로젝트
-  char subject[100] // 과목
+  char type[100]; // 과제 or 시험 or 팀프로젝트
+  char subject[100]; // 과목
 } Calendar;
 
 int selectMenu();
-int loadFile(Calender *s); //파일 불러오기
-void loadData(Calender *s, int index); //파일 저장하기
+int loadFile(Calendar *s); //파일 불러오기
+void loadData(Calendar *s, int index); //파일 저장하기
 
-int createCalendar(Calender *s);
-void readCalendar(Calender *s);
-void updateCalendar(Calender *s);
-void deleteCalendar(Calender *s);
+int createCalendar(Calendar *s);
+void readCalendar(Calendar *s);
+void updateCalendar(Calendar *s);
+void deleteCalendar(Calendar *s);
 
 int selectDataNo(Calendar *s, int count);
 void listCalender(Calendar *s, int count);
