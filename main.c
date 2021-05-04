119e65b054c88e19f17b365b252f833c575f317e
siyoon <ysi981123@hanmail.net>
2021-05-04 21:08:17 +0900

    최종 수정

--- main.c
+++ main.c
@@ -10,14 +10,14 @@ int main (){
     while(exit){
         menu = selectMenu();
         if(menu == 0) {
-            printf("í”„ë¡œê·¸ë¨ì´ ì¢…ë£Œë©ë‹ˆë‹¤!\n");
-            exit = 0; //í”„ë¡œê·¸ë¨ ì¢…ë£Œ
+            printf("ÇÁ·Î±×·¥ÀÌ Á¾·áµË´Ï´Ù!\n");
+            exit = 0; //ÇÁ·Î±×·¥ Á¾·á
         }
         if(menu ==1||menu==3||menu==4||menu ==5)
         {
             if(count == 0) 
             {
-                printf("ì €ì¥ëœ ì¼ì •ì´ ì—†ìŠµë‹ˆë‹¤\n");
+                printf("ÀúÀåµÈ ÀÏÁ¤ÀÌ ¾ø½À´Ï´Ù\n");
                 continue;
             }
         }
@@ -37,7 +37,7 @@ int main (){
             int no = selectDataNo(p,index);
             if(no>0){
                 int ok;
-                printf("ì •ë§ë¡œ ì‚­ì œí•˜ì‹œê² ìŠµë‹ˆê¹Œ?(ì‚­ì œ:1)");
+                printf("Á¤¸»·Î »èÁ¦ÇÏ½Ã°Ú½À´Ï±î?(»èÁ¦:1)");
                 scanf(" %d",&ok);
                 if(ok == 1){
                     deletePlan(&p[no-1]);
@@ -48,12 +48,12 @@ int main (){
         else if (menu == 5){
             int check = -1;
             while(check == 1 || check == 2){    
-                printf("ì €ì¥ëœ ì¼ì •ì„ ê²€ìƒ‰í•©ë‹ˆë‹¤\n");
-                printf("ë¶„ë¥˜ë¡œ ê²€ìƒ‰(ì‹œí—˜/ê³¼ì œ/íŒ€í”Œ): 1\në‚ ì§œë¡œ ê²€ìƒ‰: 2\n");
-                printf("ì›í•˜ì‹œëŠ” ê²€ìƒ‰ë°©ë²•ì„ ì…ë ¥í•´ì£¼ì„¸ìš”(ì¢…ë£Œ: 0)");
+                printf("ÀúÀåµÈ ÀÏÁ¤À» °Ë»öÇÕ´Ï´Ù\n");
+                printf("ºĞ·ù·Î °Ë»ö(½ÃÇè/°úÁ¦/ÆÀÇÃ): 1\n³¯Â¥·Î °Ë»ö: 2\n");
+                printf("¿øÇÏ½Ã´Â °Ë»ö¹æ¹ıÀ» ÀÔ·ÂÇØÁÖ¼¼¿ä(Á¾·á: 0)");
                 scanf("%d",&check);
-                if(check == 0) continue; //0 ì…ë ¥ì‹œ ì²˜ìŒìœ¼ë¡œ
-                if(check!=1 || check!=2) printf("ì˜ëª» ì…ë ¥í•˜ì…¨ìŠµë‹ˆë‹¤. ë‹¤ì‹œ ì…ë ¥í•´ì£¼ì„¸ìš”\n");
+                if(check == 0) continue; //0 ÀÔ·Â½Ã Ã³À½À¸·Î
+                if(check!=1 || check!=2) printf("Àß¸ø ÀÔ·ÂÇÏ¼Ì½À´Ï´Ù. ´Ù½Ã ÀÔ·ÂÇØÁÖ¼¼¿ä\n");
             }
             if(check==1){
                 searchPlan_type(p,count);
