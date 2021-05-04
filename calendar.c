d73179ddcbd4bd45bcd1245c15123ac92dd27ce6
siyoon <ysi981123@hanmail.net>
2021-05-04 19:17:06 +0900

    함수 이름수정(Calender -> Plan)

--- calendar.c
+++ calendar.c
@@ -1,21 +1,16 @@
-typedef struct{
-  int year; //년
-  int month;//월
-  int day;//일
-  char type[100]; // 과제 or 시험 or 팀프로젝트
-  char subject[100]; // 과목
-} Calendar;
+#include "calendar.h"
 
 int selectMenu(){
     int menu;
     printf("\n******* 한동 A+ 도우미 *******\n");
-    printf("1. 일정 조회\n");
+    printf("1. 전체 일정 조회\n");
     printf("2. 일정 추가\n");
     printf("3. 일정 수정\n");
     printf("4. 일정 삭제\n");
-    printf("5. 달력 출력\n");
+    printf("5. 일정 검색\n");
+    printf("5. 달력 보기\n");
     printf("0. 종료\n");
-    printf("******* 한동 A+ 도우미 *******\n");
+    printf("********************************\n");
     printf("==> 원하는 메뉴는 무엇입니까? ");
     scanf(" %d",&menu);
     return menu;
@@ -25,7 +20,7 @@ void clearbuffer(){
     while (getchar() != '\n');
 }
 
-int createCalendar(Calendar *p){
+int create_plan(Calendar *p){
     fputs("년: ",stdout);
     clearbuffer();
     scanf("%d",&p->year);
@@ -42,30 +37,30 @@ int createCalendar(Calendar *p){
     return 1;
 } // 일정 추가 함수 
 
-int readCalendar(Calendar p){
+int readPlan(Calendar p){
     printf(" %d년  %d월 \t %d일 \t\t %s \t (%s)\n",p.year,p.month,p.day,p.type,p.subject);
     return 1;
 }// 일정 read 함수 
 
-void listCalendar(Calendar *p, int count){
+void listPlan(Calendar *p, int count){
     int i = 0;
     printf("\nNo \t 년 \t 월 \t 일 \t\t 종류 \t 과목\n");
     for(i = 0; i < count; i++){
         if(p[i].year == -1) continue;
         printf("%d \t",i+1);
-        readCalendar(p[i]);
+        readPlan(p[i]);
     }
 } // 전체 일정 출력 함수
 
 int selectDataNo(Calendar *p, int count){
     int num;
-    listCalendar(p,count);
+    listPlan(p,count);
     printf("선택할 번호는 무엇입니까?(취소:0) ");
     scanf(" %d",&num);
     return num;
 } // data 선택 함수 
 
-int updateCalendar(Calendar *p){
+int updatePlan(Calendar *p){
     fputs("수정할 년도: ",stdout);
     clearbuffer();
     scanf("%d",&p->year);
@@ -82,7 +77,7 @@ int updateCalendar(Calendar *p){
     return 1;
 } // 일정 수정 함수 
 
-int deleteCalendar(Calendar *s){
+int deletePlan(Calendar *s){
     s->year = -1;
     s->month = -1;
     s-> day = -1;
--- calendar.h
+++ calendar.h
@@ -13,15 +13,15 @@ int selectMenu();
 int loadFile(Calendar *s); //파일 불러오기
 void loadData(Calendar *s, int index); //파일 저장하기
 
-int createCalendar(Calendar *s);
-void readCalendar(Calendar *s);
-void updateCalendar(Calendar *s);
-void deleteCalendar(Calendar *s);
+int createPlan(Calendar *s);
+void readPlan(Calendar *s);
+void updatePlan(Calendar *s);
+void deletePlan(Calendar *s);
 
 int selectDataNo(Calendar *s, int count);
-void listCalender(Calendar *s, int count);
+void listPlan(Calendar *s, int count);
 
-void searchCalender_category(Calendar *s, int count);
-void searchCalender_month(Calendar *s, int count);
+void searchPlan_category(Calendar *s, int count);
+void searchPlan_month(Calendar *s, int count);
 
 void showCalendar();
