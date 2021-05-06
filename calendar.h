467be7c9c3f0bd452a84a45ba9e6a48e4cb703a3
hasung98 <75055715+hasung98@users.noreply.github.com>
2021-05-06 12:44:01 +0900

    한글 test

--- calendar.h
+++ calendar.h
@@ -6,11 +6,11 @@
 #define NO 0
 
 typedef struct{
-  int year; //³â
-  int month;//¿ù
-  int day;//ÀÏ
-  int type; // °úÁ¦ or ½ÃÇè or ÆÀÇÁ·ÎÁ§Æ®
-  char subject[100]; // °ú¸ñ
+  int year; //ë…„
+  int month;//ì›”
+  int day;//ì¼
+  int type; // ê³¼ì œ or ì‹œí—˜ or íŒ€í”„ë¡œì íŠ¸
+  char subject[100]; // ê³¼ëª©
 } Calendar;
 
 int selectMenu();
@@ -32,5 +32,5 @@ int is_leap_year(int x);
 int month_days(int y, int m);
 int first_day(int y, int m);
 
-int loadData(Calendar *s); //ÆÄÀÏ ºÒ·¯¿À±â
-void saveData(Calendar *s, int index); //ÆÄÀÏ ÀúÀåÇÏ±â
+int loadData(Calendar *s); //íŒŒì¼ ë¶ˆëŸ¬ì˜¤ê¸°
+void saveData(Calendar *s, int index); //íŒŒì¼ ì €ì¥í•˜ê¸°
